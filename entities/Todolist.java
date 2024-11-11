29517c6f55463458bf368f0168ff3c1d90db0a0e
Joshua Pangihutan Sinabutar <2381018@unai.edu>
2024-11-11 21:42:41 +0700

    Handson 8

---
+++ entities/Todolist.java
@@ -0,0 +1,28 @@
+package entities;
+
+public class TodoList {
+    private String todo;
+    private Integer id;
+
+    public TodoList() {
+    }
+
+    public String getTodo() {
+        return todo;
+    }
+
+    public void setTodo(final String todo) {
+        this.todo = todo;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public void add(TodoList todoList) {
+    }
+}
